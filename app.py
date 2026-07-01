20f6f4d79aebb03bf78e9fa5478653f548019168
jyotshna068 <gavireddyjyotyshnadevi@gmail.com>
2026-07-01 16:44:53 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_login import current_user
 
 from config import Config
 from extensions import db, login_manager, mail
@@ -44,10 +45,27 @@ def create_app():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 
 app = create_app()
+
+@app.route("/")
+def home():
+
+    if current_user.is_authenticated:
+
+        if current_user.role == "student":
+            return redirect(url_for("student.dashboard"))
+
+        elif current_user.role == "organizer":
+            return redirect(url_for("organizer.dashboard"))
+
+        elif current_user.role == "admin":
+            return redirect(url_for("admin.dashboard"))
+
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
